a6b75fad05bcd993bf1a4b8b74ddf2830831b42e
Willian Andrade <willianandrade953@gmail.com>
2021-06-29 13:16:31 -0300

    Removed all console.log

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ const leftTarget = document.querySelectorAll('.left');
 const rightTarget = document.querySelectorAll('.right');
 
 function scrollAnimation() {
-    const topSize = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    const topSize = window.pageYOffset;
     console.log(topSize);
     leftTarget.forEach(element => {
         if (topSize > element.offsetTop) {
-            console.log(element.offsetTop);
             element.style.animation = 'insertAnim 2s forwards'
-        } else {
-
         }
     });
     rightTarget.forEach(element => {
         if (topSize > element.offsetTop) {
-            console.log(element.offsetTop);
             element.style.animation = 'insertAnim 2s forwards'
         }
     });
